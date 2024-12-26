3d9553d94335e4162077978e79db65386a131f42
Andrea Grandi <andrea.grandi@tiscali.it>
2024-12-27 00:33:18 +0100

    minor updates

--- src/bio_project/main.py
+++ src/bio_project/main.py
@@ -18,7 +18,7 @@ def set_seed(seed: int = 42) -> None:
   os.environ['TF_DETERMINISTIC_OPS'] = '1'
   os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
   os.environ['TF_DISABLE_SPARSE_SOFTMAX_XENT_WITH_LOGITS_OP_DETERMINISM_EXCEPTIONS']='1'
-
+  
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Train APKMIL')
