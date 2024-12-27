e47b11ce5544b2894e50c729f40935c12852238c
Andrea Grandi <andrea.grandi@tiscali.it>
2024-12-27 15:15:36 +0100

    minor updates

--- src/bio_project/tests/features_extractor.py
+++ src/bio_project/tests/features_extractor.py
@@ -1,25 +1,27 @@
 import torch
 import pandas as pd
-from torchvision.models import resnet50
+from torchvision.models import resnet50, ResNet50_Weights
 from torchvision.transforms import Compose, ToTensor, Normalize
 from PIL import Image
+import ast
 
 # Pretrained ResNet50
-resnet = resnet50(pretrained=True)
+weights = ResNet50_Weights.IMAGENET1K_V1 
+model = resnet50(weights=weights)
 # Remove last layer
-resnet = torch.nn.Sequential(*list(resnet.children())[:-1]) 
+resnet = torch.nn.Sequential(*list(model.children())[:-1]) 
 resnet.eval()
 
 # Metadata
-metadata = pd.read_csv('selected_patches_from_camelyon17/metadata.csv')
-extracted_metadata = pd.read_csv('selected_patches_from_camelyon17/extracted_metadata.csv')
+metadata = pd.read_csv('selected_patches_from_camelyon17/final_metadata.csv')
 
 # Patches
 image_patch_path = "selected_patches_from_camelyon17/"
 
-# Normalization of numerical features
-for col in extracted_metadata.columns[1:]:
-    extracted_metadata[col] = (extracted_metadata[col] - extracted_metadata[col].mean()) / extracted_metadata[col].std()
+def normalize():
+    # Normalization of numerical features
+    for col in metadata.columns[1:]:
+        metadata[col] = (metadata[col] - metadata[col].mean()) / metadata[col].std()
 
 # Pipeline of image transformation
 transform = Compose([ToTensor(), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
@@ -34,13 +36,23 @@ def extract_features(image_path):
 # Combine visual features and numerical ones
 combined_features = []
 for idx, row in metadata.iterrows():
-	image_filename = f"patch_patient_{str(row['patient']).zfill(3)}_node_{row['node']}_x_{row['x']}_y_{row['y']}.png"
-	print(image_filename)
-    image_path = f"{image_patch_path}/{image_filename}"
+    image_filename = f"patch_patient_{str(int(row['patient'])).zfill(3)}_node_{int(row['node'])}_x_{int(row['x_coord'])}_y_{int(row['y_coord'])}.png"
+    image_path = f"{image_patch_path}{image_filename}"
+    normalize()
     visual_features = extract_features(image_path)
-    numerical_features = torch.tensor(extracted_metadata.loc[idx, extracted_metadata.columns[1:]].values, dtype=torch.float32)
+    if idx in metadata.index:
+        numerical_features = torch.tensor(
+            metadata.loc[idx, metadata.columns[1:]].values, dtype=torch.float32
+        )
+    else:
+        print(f"Indice {idx} non trovato in metadata.")
+
+    # concat visual features with numerical features
     combined = torch.cat((visual_features, numerical_features))
     combined_features.append(combined)
 
 # to tensor
 combined_features = torch.stack(combined_features)
+print(combined_features.shape) # (1000, 2058)
+print(combined_features[0]) 
+print("Done")
--- src/bio_project/tests/merge_csv_metadata.py
+++ src/bio_project/tests/merge_csv_metadata.py
@@ -1,22 +1,30 @@
 import pandas as pd
 
-metadata_camelyon_path = "camelyon17_v1.0/metadata.csv"
-metadata_seg_path = "cells_segmentation_output/metadata.csv"
+metadata_camelyon_path = "selected_patches_from_camelyon17/metadata.csv"
+metadata_seg_path = "selected_patches_from_camelyon17/extracted_metadata.csv"
 
 df1 = pd.read_csv(metadata_camelyon_path)
 df2 = pd.read_csv(metadata_seg_path)
 
-df2.rename(columns={
+df2_copy = df2.copy()
+df2_copy = df2_copy.drop(columns=['bounding_box', 'area'])
+
+df2_copy = df2_copy.groupby(list(df2_copy.columns)).size().reset_index(name='counts')
+df2_copy = df2_copy.drop(columns='counts')
+print(df2_copy.head())
+
+
+df2_copy.rename(columns={
     "Patient": "patient",
     "Node": "node",
     "X_coord": "x_coord",
     "Y_coord": "y_coord"
     }, inplace=True)
 
-merged_df = pd.merge(df1, df2, on=["patient", "node", "x_coord", "y_coord"], how="left")
+merged_df = pd.merge(df1, df2_copy, on=["patient", "node", "x_coord", "y_coord"], how="left")
 
 # Path to output metadata
-output_path = "final_metadata.csv"
+output_path = "selected_patches_from_camelyon17/final_metadata.csv"
 merged_df.to_csv(output_path, index=False)
 
 print(f"Merged completed. Output file saved in {output_path}")
