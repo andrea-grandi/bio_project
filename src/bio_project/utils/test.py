49b6f5a5976fb5cbc901a2e47c74a25670134cad
Andrea Grandi <andrea.grandi@tiscali.it>
2025-01-28 12:22:47 +0100

    update

--- src/bio_project/utils/test.py
+++ src/bio_project/utils/test.py
@@ -31,25 +31,28 @@ def test(model, testloader):
         data = data.cuda()
         x, edge_index, childof, level = data.x, data.edge_index, data.childof, data.level
 
+        # Extract additional features (cellpose)
+        num_cells = data.num_cells
+        cell_density = data.cell_density
+        mean_cell_area = data.mean_cell_area
+
         # Check if additional edge indices are present
         if data.__contains__("edge_index_2") and data.__contains__("edge_index_3"):
             edge_index2, edge_index3 = data.edge_index_2, data.edge_index_3
         else:
             edge_index2 = None
             edge_index3 = None
+            
         # Forward pass through the model
-        results = model(x, edge_index, level, childof,
-                        edge_index2, edge_index3)
+        results = model(x, edge_index, level, childof, edge_index2, edge_index3)
         bag_label = data.y.float().squeeze().cpu().numpy()
 
         # Convert bag label to numpy array
         if model.classes == 2:
             if bag_label == 1:
-                bag_label = torch.LongTensor(
-                    [[0, 1]]).float().squeeze().cpu().numpy()
+                bag_label = torch.LongTensor([[0, 1]]).float().squeeze().cpu().numpy()
             else:
-                bag_label = torch.LongTensor(
-                    [[1, 0]]).float().squeeze().cpu().numpy()
+                bag_label = torch.LongTensor([[1, 0]]).float().squeeze().cpu().numpy()
 
         # Append the test labels and predictions
         test_labels.extend([bag_label])
@@ -57,8 +60,7 @@ def test(model, testloader):
         test_predictions0.extend([(preds[0]).squeeze().cpu().detach().numpy()])
 
         if preds[1] is not None:
-            test_predictions1.extend(
-                [(preds[1]).squeeze().cpu().detach().numpy()])
+            test_predictions1.extend([(preds[1]).squeeze().cpu().detach().numpy()])
 
     # Convert the test labels and predictions to numpy arrays
     test_labels = np.array(test_labels)
@@ -66,13 +68,11 @@ def test(model, testloader):
     test_predictions1 = np.array(test_predictions1)
 
     # Compute metrics for higher-level predictions
-    avg_score_higher, auc_value_higher, class_prediction_bag_higher = computeMetrics(
-        test_labels, test_predictions0, model.classes, names)
+    avg_score_higher, auc_value_higher, class_prediction_bag_higher = computeMetrics(test_labels, test_predictions0, model.classes, names)
 
     # Compute metrics for lower-level predictions if available
     if test_predictions1.shape[0] != 0:
-        avg_score_lower, auc_value_lower, class_prediction_bag_lower = computeMetrics(
-            test_labels, test_predictions1, model.classes, names)
+        avg_score_lower, auc_value_lower, class_prediction_bag_lower = computeMetrics(test_labels, test_predictions1, model.classes, names)
     else:
         avg_score_lower = 0
         auc_value_lower = 0
